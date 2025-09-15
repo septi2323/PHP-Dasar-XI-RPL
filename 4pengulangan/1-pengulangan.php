28a76f14c46a9a602774214be72b71c7a8e32c3c
septi2323 <septianramdani062@gmail.com>
2025-09-15 13:42:52 +0700

    pengulangan

---
+++ 4pengulangan/1-pengulangan.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pengulangan</title>
+</head>
+<body>
+	<?php 
+		// $i =;
+		for ($i=1; $i <=10 ; $i++) {
+			echo "$i. ini akan tampil sebanyak 10X";
+			echo "<br>";
+		}
+	 ?>
+
+</body>
+</html>
