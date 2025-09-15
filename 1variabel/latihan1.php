581d51246d8c4c03a8bf5060fcb530b9969705f1
septi2323 <septianramdani062@gmail.com>
2025-09-15 13:39:33 +0700

    variabel

---
+++ 1variabel/latihan1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="widht=device-width, initial-scale=1">
+	<title>variabel</title>
+</head>
+<body>
+	<?php 
+		$nama="septi";
+
+		echo $nama;
+	 ?>
+</body>
+</html>
---
+++ 1variabel/latihan2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>variabel latihan 2</title>
+</head>
+<body>
+	<?php 
+		$nama="septi";
+		$kelas="11 RPL";
+		$alamat="subang. kp,cibogo. des,cibogo.";
+
+		echo "nama : $nama";
+		echo "<br>";
+		echo "kelas : $kelas";
+		echo "<br>";
+		echo "alamat : $alamat";
+	 ?>
+</body>
+</html>
